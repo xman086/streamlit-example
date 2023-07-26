081bdff5e320e03198769bdd2c996cc929c031c6
xman086 <gotohuangzhs@gmail.com>
2023-07-26 16:44:27 +0800

    Update 01chat.py

--- pages/01chat.py
+++ pages/01chat.py
@@ -1,1 +1,16 @@
 
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+
+col1, col2 = st.beta_columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
