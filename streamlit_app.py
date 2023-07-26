52dfadccd93966d89a4e534947bcf7b37fe46b58
xman086 <gotohuangzhs@gmail.com>
2023-07-26 15:52:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,14 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-import webbroswer
+
 
 col1, col2, col3 = st.columns(3)
 
 
 if st.button('打开新页面'):
     url = '/mypages/chat.py'
-    webbrowser.open(url)
+    st.write(url)
 
 with col1:
     st.header("A cat")
